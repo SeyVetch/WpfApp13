39e393a8754761a27f69536918ac794645b2a5ff
Александр Маханов <dendiherobrine@gmail.com>
2021-04-06 12:44:40 +0300

    Добавьте файлы проекта.

---
+++ WpfApp13/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string LName = txtLName.Text;
+            string FName = txtFName.Text;
+            string Age = txtAge.Text;
+
+            if (string.IsNullOrWhiteSpace(LName))
+            {
+                MessageBox.Show("Введите фамилию");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(FName))
+            {
+                MessageBox.Show("Введите имя");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(Age))
+            {
+                MessageBox.Show("Введите возраст");
+                return;
+            }
+            uint val;
+            if (!uint.TryParse(Age, out val))
+            {
+                MessageBox.Show("Неверный возраст");
+                return;
+            }
+            using (StreamWriter sw = new StreamWriter("data.txt", true))
+            {
+                sw.WriteLine($"{LName};{FName};{Age}");
+            }
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            this.Hide();
+            w.ShowDialog();
+            this.Show();
+        }
+    }
+}
---
+++ WpfApp13/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        List<Person> people;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("data.txt"))
+            {
+                people = new List<Person>();
+                string line;
+                int i = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Person p = new Person();
+                    p.Id = i + 1;
+                    string[] data = line.Split(';');
+                    p.LastName = data[0];
+                    p.FirstName = data[1];
+                    p.Age = int.Parse(data[2]);
+                    people.Add(p);
+                    line = sr.ReadLine();
+                    i++;
+                }
+                DataGridUsers.ItemsSource = people;
+            }
+        }
+    }
+}
